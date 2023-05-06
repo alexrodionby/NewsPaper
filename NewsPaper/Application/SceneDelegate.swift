8d88e7661c20e935a6671c4689a2712c7b59a1ac
Alexandr Rodionov <alexrodionby@gmail.com>
2023-05-06 16:59:57 +0300

    added the file structure of the project, removed the storyboard

---
+++ NewsPaper/Application/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  NewsPaper
+//
+//  Created by Alexandr Rodionov on 6.05.23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let controller = UINavigationController(rootViewController: MainViewController())
+        window?.rootViewController = controller
+        window?.makeKeyAndVisible()
+    }
+}
--- NewsPaper/Screens/Main/MainViewController.swift
+++ NewsPaper/Screens/Main/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  NewsPaper
 //
 //  Created by Alexandr Rodionov on 6.05.23.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
-
