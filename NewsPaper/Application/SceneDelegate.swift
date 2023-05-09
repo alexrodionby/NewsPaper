6e04193203228dc91bc89a78b2ca69dc3d651870
iosDevelopForYou <iosdevelopforyou@gmail.com>
2023-05-09 15:36:17 +0400

    add Profile content and link buttons

--- NewsPaper/Application/SceneDelegate.swift
+++ NewsPaper/Application/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let controller = UINavigationController(rootViewController: DetailNewsViewController())
+        let controller = UINavigationController(rootViewController: ProfileViewController())
         window?.rootViewController = controller
         window?.makeKeyAndVisible()
     }
---
+++ NewsPaper/Screens/Profile/TermsConditionsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TermsConditionsViewController.swift
+//  NewsPaper
+//
+//  Created by Marat Guseynov on 09.05.2023.
+//
+
+import UIKit
+
+class TermsConditionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .darkGray
+    }
+}
