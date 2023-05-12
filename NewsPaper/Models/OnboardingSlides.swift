86d3ac13b6d605a94e5bc91c46a5fb7726ec5a69
Larisa Teregulova <larisa.andreeva.1995@mail.ru>
2023-05-12 22:46:09 +0300

    add new image and color

---
+++ NewsPaper/Models/OnboardingSlides.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingModel.swift
+//  NewsPaper
+//
+//  Created by Лариса Терегулова on 12.05.2023.
+//
+
+import UIKit
+
+struct OnboardingModel {
+    let image: UIImage
+    let pageControl: UIPageControl
+    let title: String
+    let description: String
+}
---
+++ NewsPaper/Screens/Onboarding/OnboardingCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingCollectionViewCell.swift
+//  NewsPaper
+//
+//  Created by Лариса Терегулова on 12.05.2023.
+//
+
+import UIKit
+
+class OnboardingCollectionViewCell: UICollectionViewCell {
+    
+}
